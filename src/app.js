36c282a38cb7d798b01afc11e67eb10cd60faf8a
James McDaniel <icathaid@gmail.com>
2018-06-09 18:19:45 -0700

    POST route working (maybe?) need to write tests

--- src/app.js
+++ src/app.js
@@ -8,27 +8,11 @@
 // 1st Party library
 const http = require('http');
 const cowsay = require('cowsay');
-// const promisify = require('./lib/promisify');
 // Local Libraries
-// parser will tear the URL apart and give us back an object with things like path, query params, etc.
-// it will also deal with POST data and append JSON to req.body if sent
 const parser = require('./lib/parser');
-
 const requestHandler = (req,res) => {
-
-  // Take a look here if you're interested to see what some parts of the request object are.
-  // console.log(req.method);
-  // console.log(req.headers);
-  // console.log(req.url);
-
-  // In all cases, parse the URL
   parser(req)
     .then( req => {
-
-      /* The "if" statements below are our "routes" and do the same things that express does (below) but 100% manually
-           app.get('/', (req,res) => res.send('Hello From the Gutter'));
-           app.get('/foo/bar/baz', (req,res) => res.send('Hello From the Gutter'));
-      */
       if ( req.method === 'GET' && req.url.pathname === '/' ) {
         res.setHeader('Content-Type', 'text/html');
         res.statusCode = 200;
@@ -42,7 +26,7 @@ const requestHandler = (req,res) => {
         res.statusCode = 200;
         res.statusMessage = 'OK';
         if(!req.url.query.text){
-          res.write(cowsay.say({text: 'stuff'}));
+          res.write(cowsay.say({text: 'Give me something to say'}));
         } else {
           res.write(cowsay.say({text: req.url.query.text}));
         };
@@ -52,11 +36,12 @@ const requestHandler = (req,res) => {
   
       // Here, we have a "POST" request which will always return a JSON object.  That object will either be
       // the JSON that you posted in (just spitting it back out), or an error object, formatted to look like JSON
-      if ( req.method === 'POST' && req.url.pathname === '/data' ) {
+
+      if ( req.method === 'POST' && req.url.pathname === '/cowsay' ) {
         res.setHeader('Content-Type', 'text/json');
         res.statusCode = 200;
         res.statusMessage = 'OK';
-        res.write( JSON.stringify(req.body) );
+        res.write( JSON.stringify({content: cowsay.say({text: req.body.text})}));
         res.end();
         return;
      }
@@ -69,7 +54,6 @@ const requestHandler = (req,res) => {
         return;
       }
     })
-     // closes the "then" of the parser promise
     .catch(err => {
       res.writeHead(500);
       res.write(err);
